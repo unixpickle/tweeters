c9ba0f68a264290343ce00376dc80f4c73496af9
Alex Nichol <unixpickle@gmail.com>
2017-03-12 20:02:22 -0400

    read samples from CSV file
    With the default GOGC setting and 3M twitter users, this takes >5 minutes to run and uses around 5.5GB of memory.

---
+++ samples.go
@@ -0,0 +1,92 @@
+package tweetures
+
+import (
+	"bytes"
+	"encoding/csv"
+	"errors"
+	"io"
+	"os"
+
+	"github.com/unixpickle/essentials"
+)
+
+// Samples stores a list of users and tweets by those
+// users.
+type Samples struct {
+	Users  []string
+	Tweets map[string][][]byte
+}
+
+// ReadSamples loads tweets from a CSV file.
+// The file should be formatted like the outputs of
+// tweetdump: https://github.com/unixpickle/tweetdump.
+func ReadSamples(csvPath string) (samples *Samples, err error) {
+	defer essentials.AddCtxTo("read samples", &err)
+
+	f, err := os.Open(csvPath)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+
+	// Reduce memory consumption by counting tweets/user
+	// before recording any tweet bodies in memory.
+
+	counts := map[string]int{}
+	for {
+		row, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+		if len(row) < 3 {
+			return nil, errors.New("expected at least 3 columns")
+		}
+		counts[row[1]]++
+	}
+
+	if _, err := f.Seek(0, io.SeekStart); err != nil {
+		return nil, err
+	}
+	reader = csv.NewReader(f)
+
+	// Read every tweet body into a single buffer, then slice
+	// it up into individual tweets.
+	// This avoids some memory fragmentation, although not a
+	// lot as far as I can tell.
+
+	samples = &Samples{Tweets: map[string][][]byte{}}
+	indices := map[string][]int{}
+	buffer := bytes.Buffer{}
+	for {
+		row, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		} else if counts[row[1]] < 2 {
+			continue
+		}
+		user := row[1]
+		if _, ok := indices[user]; !ok {
+			samples.Users = append(samples.Users, user)
+		}
+		msg := []byte(row[len(row)-1])
+		indices[user] = append(indices[user], buffer.Len(), buffer.Len()+len(msg))
+		buffer.Write(msg)
+	}
+
+	fullBytes := buffer.Bytes()
+	for user, is := range indices {
+		var strs [][]byte
+		for i := 0; i < len(is); i += 2 {
+			strs = append(strs, fullBytes[is[i]:is[i+1]])
+		}
+		samples.Tweets[user] = strs
+	}
+
+	return
+}
